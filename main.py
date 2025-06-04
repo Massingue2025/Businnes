d0620df1aba690f3baedd321244db3f03cfb034f
Massingue2025 <antoniocarlosmassingue20@gmail.com>
2025-06-04 18:19:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import httpx
+import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+API_PORT = os.getenv("API_PORT", "21465")
+API_URL = f"http://localhost:{API_PORT}/api/message/send-text"
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
@@ -12,11 +16,10 @@ async def index(request: Request):
 
 @app.post("/send")
 async def send(number: str = Form(...), message: str = Form(...)):
-    url = "http://localhost:21465/api/message/send-text"
     payload = {"phone": number, "message": message}
     async with httpx.AsyncClient() as client:
         try:
-            r = await client.post(url, json=payload)
-            return {"ok": True, "resposta": r.json()}
+            response = await client.post(API_URL, json=payload)
+            return {"ok": True, "resposta": response.json()}
         except Exception as e:
             return {"ok": False, "erro": str(e)}
